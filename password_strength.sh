2b3876cb1ae8767613939d131dc88baabd1cc3b5
mssoheil <mssoheil88@yahoo.com>
2024-01-09 09:39:45 +0330

    fix: special chars password issue

--- password_strength.sh
+++ password_strength.sh
@@ -5,18 +5,23 @@ read -p "please enter your password: " password
 rate=0
 
 length=$(echo -n $password | wc -c)
-numeric=$(echo -n $password | egrep -o '[0-9]+' | wc -c)
-alpha=$(echo -n $password | egrep -o '[a-zA-Z]+' | wc -c)
-special=$(echo -n $password | egrep -o '[`~!@#$%^&*()_+=,./\\|\\;\:\]\}\[\{\"\']+' | wc -c)
+numeric=$(echo -n $password | egrep -o '[0-9]' | wc -l)
+alpha=$(echo -n $password | egrep -o '[a-zA-Z]' | wc -l)
+special=$(echo -n $password | egrep -o '[-+=_)(*&^%$#@!~`<,>.?/]' | wc -l)
 
 
 if [[ $length -gt 8 ]]; then
 	(( rate += ( ( $length - 8) * 3 ) ))
 fi
 
-echo "numeric count: $numeric"
+(( rate += ($special) * 4 ))
 
-echo $rate
+if [[ $numeric -gt 1 && $alpha -gt 1 ]]; then
+	(( rate += ($numeric * $alpha) ))
+fi
+
+
+echo "The password strength rate is: $rate"
 
 
 
