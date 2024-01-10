7e019f26ba363191d536f89a563132914ec27d3a
mssoheil <mssoheil88@yahoo.com>
2024-01-10 09:46:49 +0330

    feat: use function to calculate rate

--- password_strength.sh
+++ password_strength.sh
@@ -2,27 +2,53 @@
 
 read -p "please enter your password: " password
 
-rate=0
-
 length=$(echo -n $password | wc -c)
-numeric=$(echo -n $password | egrep -o '[0-9]' | wc -l)
-alpha=$(echo -n $password | egrep -o '[a-zA-Z]' | wc -l)
-special=$(echo -n $password | egrep -o '[-+=_)(*&^%$#@!~`<,>.?/]' | wc -l)
 
+calculateRate() {
+	local rate=0
+	numeric=$(echo -n $password | egrep -o '[0-9]' | wc -l)
+	alpha=$(echo -n $password | egrep -o '[a-zA-Z]' | wc -l)
+	special=$(echo -n $password | egrep -o '[-+=_)(*&^%$#@!~`<,>.?/\ ]' | wc -l)
 
-if [[ $length -gt 8 ]]; then
-	(( rate += ( ( $length - 8) * 3 ) ))
+	if [[ $length -gt 8 ]]; then
+		(( rate += ( ( $length - 8) * 3 ) ))
+	fi
+
+	(( rate += ($special) * 4 ))
+
+	if [[ $numeric -gt 1 && $alpha -gt 1 ]]; then
+		(( rate += ($numeric * $alpha) ))
+	fi
+
+	echo $rate
+	return $rate
+}
+
+calculateRate
+calculatedRate=$?
+
+if [[ $length -lt 8 ]]; then
+	echo "The password strength rate is $calculatedRate.The password is very weak. You should change it immediately"
+	exit 1
 fi
 
-(( rate += ($special) * 4 ))
-
-if [[ $numeric -gt 1 && $alpha -gt 1 ]]; then
-	(( rate += ($numeric * $alpha) ))
+if [[ $calculatedRate -gt 8 && $calculatedRate -lt 16 ]]; then
+	echo "The password strength rate is $calculatedRate. It's fairly weak password."
+	exit 1
 fi
 
+if [[ $calculatedRate -gt 16 && $calculatedRate -lt 32 ]]; then
+	echo "The password strength rate is $calculatedRate. It has a moderate strength."
+	exit 1
+fi
 
-echo "The password strength rate is: $rate"
+if [[ $calculatedRate -gt 32 && $calculatedRate -lt 40 ]]; then
+	echo "The password strength rate is $calculatedRate. It's a strong password."
+	exit 1
+fi
 
-
-
+if [[ $calculatedRate -gt 40 ]]; then
+	echo "The password strength rate is $calculatedRate. It's a vary strong password."
+	exit 1
+fi
 
